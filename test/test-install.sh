0cb5d7f81ad85be4c1c30282575e783922b3f638
Denis Benato <benato.denis96@gmail.com>
2024-06-22 15:42:26 +0200

    fix test

--- test/test-install.sh
+++ test/test-install.sh
@@ -4,8 +4,10 @@ set -e
 set -x
 
 FILENAME=image.img
-BUILD_IMG="/workdir/output/$FILENAME"
+BUILD_DIR="/workdir/output"
+BUILD_IMG="$BUILD_DIR/$FILENAME"
 
+mkdir -p "$BUILD_DIR"
 dd if=/dev/zero of=$BUILD_IMG bs=1M count=4096
 
 # Associate the image file with a loop device
@@ -14,4 +16,4 @@ losetup -fP $FILENAME
 # Find the loop device associated with the image file
 MOUNTED_DEVICE=$(losetup -a | grep "$FILENAME" | cut -d ' ' -f 1 | sed 's/://')
 
-bash /workdir/frzr bootstrap $MOUNTED_DEVICE
+DISK="$MOUNTED_DEVICE" bash /workdir/frzr bootstrap $MOUNTED_DEVICE
