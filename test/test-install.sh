fb23d2f5a94be3d10a070bfda4315e0b5dc8bae4
Denis Benato <benato.denis96@gmail.com>
2024-06-22 18:04:29 +0200

    Cleanout after installation

--- test/test-install.sh
+++ test/test-install.sh
@@ -27,3 +27,9 @@ MOUNTED_DEVICE=$(losetup -a | grep "$FILENAME" | cut -d ' ' -f 1 | sed 's/://')
 
 DISK="$MOUNTED_DEVICE" SWAP_GIB=0 bash /workdir/frzr bootstrap
 SKIP_UEFI_CHECK="yes" MOUNT_PATH="/tmp/frzr_root" bash /workdir/frzr deploy chimeraos/chimeraos:unstable
+
+# Umount the loopback device
+losetup -d "$MOUNTED_DEVICE"
+
+# Remove the file
+rm -f $BUILD_IMG
