d0915183a7749af637fbc4ee468bd7c060903909
Denis Benato <benato.denis96@gmail.com>
2024-06-22 17:39:23 +0200

    test deployment

--- test/test-install.sh
+++ test/test-install.sh
@@ -25,3 +25,4 @@ losetup -fP "$BUILD_IMG"
 MOUNTED_DEVICE=$(losetup -a | grep "$FILENAME" | cut -d ' ' -f 1 | sed 's/://')
 
 DISK="$MOUNTED_DEVICE" SWAP_GIB=0 bash /workdir/frzr bootstrap
+bash /workdir/frzr deploy chimeraos/chimeraos:unstable
