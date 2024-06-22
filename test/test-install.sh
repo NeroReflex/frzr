10367edfa26441ac662c3e1395313518135a966c
Denis Benato <benato.denis96@gmail.com>
2024-06-22 19:08:00 +0200

    try to fix MOUNT_PATH not specified

--- test/test-install.sh
+++ test/test-install.sh
@@ -16,9 +16,9 @@ pacman -S --noconfirm parted btrfs-progs file libnewt dosfstools jq util-linux z
 groupadd -g 379 frzr
 usermod -a -G frzr $(whoami)
 
-FILENAME=image.img
-BUILD_DIR="/workdir/output"
-BUILD_IMG="$BUILD_DIR/$FILENAME"
+export FILENAME=image.img
+export BUILD_DIR="/workdir/output"
+export BUILD_IMG="$BUILD_DIR/$FILENAME"
 
 mkdir -p "$BUILD_DIR"
 dd if=/dev/zero of=$BUILD_IMG bs=1M count=8192
@@ -29,12 +29,18 @@ losetup -fP "$BUILD_IMG"
 # Find the loop device associated with the image file
 MOUNTED_DEVICE=$(losetup -a | grep "$FILENAME" | cut -d ' ' -f 1 | sed 's/://')
 
-DISK="$MOUNTED_DEVICE" SWAP_GIB=0 bash /workdir/frzr bootstrap
+export DISK="$MOUNTED_DEVICE"
+export SWAP_GIB=0
+bash /workdir/frzr bootstrap
 
 # Display what's mounted
 mount
 
-SKIP_UEFI_CHECK="yes" MOUNT_PATH="/tmp/frzr_root" FRZR_SKIP_CHECK="yes" SYSTEMD_RELAX_ESP_CHECKS=1 bash /workdir/frzr deploy chimeraos/chimeraos:45_1
+export SKIP_UEFI_CHECK="yes"
+export MOUNT_PATH="/tmp/frzr_root"
+export FRZR_SKIP_CHECK="yes"
+export SYSTEMD_RELAX_ESP_CHECKS=1
+bash /workdir/frzr deploy chimeraos/chimeraos:45_1
 
 # Umount the loopback device
 losetup -d "$MOUNTED_DEVICE"
