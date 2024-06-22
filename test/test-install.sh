dac4f0b6c4a90892eed934c93deb823c842bb740
Denis Benato <benato.denis96@gmail.com>
2024-06-22 15:44:48 +0200

    Fix losetup command

--- test/test-install.sh
+++ test/test-install.sh
@@ -11,9 +11,9 @@ mkdir -p "$BUILD_DIR"
 dd if=/dev/zero of=$BUILD_IMG bs=1M count=4096
 
 # Associate the image file with a loop device
-losetup -fP $FILENAME
+losetup -fP "$BUILD_IMG"
 
 # Find the loop device associated with the image file
 MOUNTED_DEVICE=$(losetup -a | grep "$FILENAME" | cut -d ' ' -f 1 | sed 's/://')
 
-DISK="$MOUNTED_DEVICE" bash /workdir/frzr bootstrap $MOUNTED_DEVICE
+DISK="$MOUNTED_DEVICE" bash /workdir/frzr bootstrap
