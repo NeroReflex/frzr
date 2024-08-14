247b265a1406634d3dcac7fa1f48e12f559e4290
Denis Benato <benato.denis96@gmail.com>
2024-08-14 18:40:50 +0200

    Revert "Use full path"
    This reverts commit a221f6b424a0fd42d682ae0dd21e77ef0343b05a.

--- test/test-permissions.sh
+++ test/test-permissions.sh
@@ -24,12 +24,12 @@ cd /workdir && make install
 frzr version
 
 # run frzr version from another user
-su - tester -c /usr/bin/frzr version
+su - tester -c frzr version
 
 sudo rm /tmp/frzr.tracker
 
 # run frzr version from tester user
-su - tester -c /usr/bin/frzr version
+su - tester -c frzr version
 
 # run frzr version from current user
 frzr version
